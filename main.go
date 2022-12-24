ae8b33896be5239d642768cd73bac1f245e6f88e
Enrique Chavez <30837445+enriquecr01@users.noreply.github.com>
2022-12-23 17:27:42 -0800

    added url example and changed port

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ func homeHandle(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello me llamo Aubert!"))
 }
 
+func exampleHandle(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Salut ç'est un exemple"))
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", homeHandle)
+	r.HandleFunc("/example", exampleHandle)
 
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":80", r)
 }
