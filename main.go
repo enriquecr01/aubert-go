b2995e2ed333722481bb94965814861eb25e115f
Enrique Chavez <30837445+enriquecr01@users.noreply.github.com>
2022-12-25 19:53:23 -0800

    changed main code to gin framework

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+	// "github.com/gorilla/mux"
 )
 
 func homeHandle(w http.ResponseWriter, r *http.Request) {
@@ -18,10 +19,19 @@ func exampleHandle(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Server initialized")
 
-	r := mux.NewRouter()
+	// r := mux.NewRouter()
 
-	r.HandleFunc("/", homeHandle)
-	r.HandleFunc("/example", exampleHandle)
+	// r.HandleFunc("/", homeHandle)
+	// r.HandleFunc("/example", exampleHandle)
 
-	http.ListenAndServe(":80", r)
+	// http.ListenAndServe(":80", r)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+
 }
