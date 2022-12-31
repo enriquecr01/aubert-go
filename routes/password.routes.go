f2d9a354ac82ad47636722425fba5c4496c41fe1
Enrique Chavez <30837445+enriquecr01@users.noreply.github.com>
2022-12-30 19:20:11 -0800

    added status in api responses

--- routes/password.routes.go
+++ routes/password.routes.go
@@ -27,11 +27,13 @@ func SearchPasswords(c *gin.Context) {
 
 	userId := c.Param("userid")
 	term := c.Param("term")
+	status := 0
 
 	where := "user_id = " + userId + " AND name LIKE '%" + term + "%' "
 	db.DB.Where(where).Find(&secrets)
 
 	c.JSON(200, gin.H{
+		"status":  status,
 		"secrets": &secrets,
 	})
 }
@@ -46,19 +48,23 @@ func AddPassword(c *gin.Context) {
 	color := c.PostForm("color")
 	userId, err := strconv.Atoi(c.PostForm("userId"))
 	message := "Registered Succesfully"
+	status := 0
 
 	if err != nil {
 		message = "Error during parsing"
+		status = 1
 	} else {
 		newSecret := models.Secret{Name: name, URL: url, Username: username, Secret: secret, Notes: notes, Color: color, UserId: userId}
 		result := db.DB.Omit("updated_at", "deleted_at").Create(&newSecret)
 
 		if result.Error != nil {
+			status = 1
 			message = "Error during insert"
 		}
 	}
 
 	c.JSON(200, gin.H{
+		"status":  status,
 		"message": message,
 	})
 }
@@ -73,9 +79,11 @@ func ModifyPassword(c *gin.Context) {
 	color := c.PostForm("color")
 	passId, err := strconv.Atoi(c.PostForm("passId"))
 	message := "Updated Succesfully"
+	status := 0
 
 	if err != nil {
 		message = "Error during parsing"
+		status = 1
 	} else {
 		updatedSecret := db.DB.Where("id = ?", passId).Updates(models.Secret{Name: name, URL: url, Username: username, Notes: notes, Secret: secret, Color: color})
 
@@ -83,6 +91,7 @@ func ModifyPassword(c *gin.Context) {
 	}
 
 	c.JSON(200, gin.H{
+		"status":  status,
 		"message": message,
 	})
 }
@@ -91,14 +100,17 @@ func DeletePassword(c *gin.Context) {
 
 	passId := c.Param("passId")
 	message := "Deleted Succesfully"
+	status := 0
 
 	deletedSecret := db.DB.Delete(&models.Secret{}, passId)
 
 	if deletedSecret.Error != nil {
 		message = "Error deleting"
+		status = 1
 	}
 
 	c.JSON(200, gin.H{
+		"status":  status,
 		"message": message,
 	})
 }
